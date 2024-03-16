84b80fc99e10cb1199ae65d35f822320c9239ca3
Byron Matthews <92761798+bmatt08@users.noreply.github.com>
2024-03-16 18:50:55 -0500

    Encryption

---
+++ Encryption/src/Main.java
@@ -0,0 +1,25 @@
+//OTP (One-Time Pad)
+		
+		/*
+		 Encryption technique that cannot be cracked,
+		 but requires the use of a one-time pre-shared key
+		 the same size as, or longer than, the message being sent.
+		 In this technique, plaintext (the message that you want to encrypt)
+		 is paired with a random secret key (also referred to as a one-time pad).
+		 */
+		
+		/*
+		 Will be replacing each letter in the message in plaintext 
+		 and substitute it with a random one from a pre-shared key that will be generated.
+		*/
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Encryption encrypt = new Encryption();
+		
+	}
+
+}
